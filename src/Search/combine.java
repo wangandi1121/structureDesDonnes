51b051e546a87b1c2aa0b3e8aef509034d89278a
wangandi1121 <819958751@qq.com>
2020-12-23 00:08:02 +0100

    12.22

--- src/Search/combine.java
+++ src/Search/combine.java
@@ -1,5 +1,37 @@
 package Search;
 
-public class combine {
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
+public class combine {
+	public List<List<Integer>> combine(int n, int k) {
+//问题有多个步骤，每个步骤有多种方法，使用回溯算法
+//回溯是在一棵树上的深度优先遍历
+		List<List<Integer>> res = new ArrayList<>();
+		if(k<=0||n<k) {
+			return res;
+		}
+		Deque<Integer> path = new ArrayDeque<>();
+		dfs(n,k,1,path,res);
+		return res;
+    }
+	private void dfs(int n,int k,int begin,Deque<Integer> path,List<List<Integer>> res) {
+		//terminator
+		if(path.size()==k) {
+			res.add(new ArrayList<>(path));
+			return;
+		}
+		//process
+		//搜索起点的上界 + 接下来要选择元素的个数 - 1 = n
+		//for(int i = begin;i<=n;i++) {
+		for(int i = begin;i<=n-(k-path.size())+1;i++) {
+			path.addLast(i);;
+			//drill down
+			dfs(n,k,i+1,path,res);
+			//深度遍历有回头的过程，因此要撤销选择
+			path.removeLast();
+		}
+	}
 }
--- src/Search/lowestCommonAncestor.java
+++ src/Search/lowestCommonAncestor.java
@@ -1,5 +1,20 @@
 package Search;
 
 public class lowestCommonAncestor {
-
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+       if(root == null||p == root||q == root) {
+    	   return root;
+       }else {
+    	   TreeNode left = lowestCommonAncestor(root.left,p,q);
+    	   TreeNode right = lowestCommonAncestor(root.right,p,q);
+    	   return left == null? right :(right == null?left:root);
+       }
+    }
+  
+}
+ class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+  TreeNode(int x) { val = x; }
 }
