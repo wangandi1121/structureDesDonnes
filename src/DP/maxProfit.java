ae7213ef0a78665e00c9a2b00e36a1b3b1bb3c99
wangandi1121 <819958751@qq.com>
2020-12-30 00:11:05 +0100

    12.29

--- src/DP/maxProfit.java
+++ src/DP/maxProfit.java
@@ -2,6 +2,11 @@ package DP;
 
 public class maxProfit {
 	 public int maxProfit(int[] prices) {
-//dp: 
+//贪心
+		 int res = 0;
+		 for(int i = 1;i<prices.length;i++) {
+			 res += Math.max(0,prices[i]-prices[i-1]);
+		 }
+		 return res;
 	    }
 }
