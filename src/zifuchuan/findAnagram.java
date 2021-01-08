083c00586c359431fa3903aea620c0239323732c
Andi WANG <andi.wang@etu.univ-nantes.fr>
2021-01-09 00:33:29 +0100

    1.8

---
+++ src/zifuchuan/findAnagram.java
@@ -0,0 +1,35 @@
+package zifuchuan;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class findAnagram {
+	public List<Integer> findAnagrams(String s, String p) {
+	//双指针滑动窗口，寻找字母异位词
+		List<Integer> ans = new ArrayList<>();
+        if(s.length() == 0 || p.length() == 0)
+            return ans;
+        int[] dict = new int[26];
+        for(int i = 0;i<p.length();i++){
+            dict[p.charAt(i)-'a'] += 1;
+        }
+       
+        int[] window = new int[26];
+        int left = 0;
+        int right = 0;
+        while(right<s.length()){
+            int curR = s.charAt(right)-'a';
+            right++;
+            window[curR] += 1;
+            while(window[curR] > dict[curR]){
+                int curL = s.charAt(left)-'a';
+                window[curL] -= 1;
+                left++;
+            }
+            if(right - left == p.length()){
+                ans.add(left);
+            }
+        }
+        return ans;
+    }
+}
